46067bb5af4b17d1af86c6eadb962ba605f66ea7
Balia077 <balaramd894@gmail.com>
2025-12-03 18:06:30 +0530

    feat: update Video component video source and enhance Agence page layout

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1764744946~exp=1764748546~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=b3407176863b4c4519dc9853f3e23366cbb738bcada4b3c9e393725ee3177542&r=dXMtZWFzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1764763072~exp=1764766672~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=589f40a8aa64667b4e5383a611e4f0f2ecb7fb638f114552545b9a9b42ad0e41&r=dXMtZWFzdDE%3D"></video>
     </div>
   )
 }
