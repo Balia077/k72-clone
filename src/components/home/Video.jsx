118b3a63c30ca7c5e64ec87ad465b0cdbca732a6
Balia077 <balaramd894@gmail.com>
2025-12-06 10:43:43 +0530

    feat: add video file and update Video component source; enhance Agence page with dynamic image loading

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1764763072~exp=1764766672~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=589f40a8aa64667b4e5383a611e4f0f2ecb7fb638f114552545b9a9b42ad0e41&r=dXMtZWFzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="/video/69496b2d.mp4"></video>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -5,23 +5,54 @@ import React, { useRef } from 'react'
 
 const Agence = () => {
 
-  const imageDivRef = useRef(null)
+
   gsap.registerPlugin(ScrollTrigger)
 
-  useGSAP(function(){
-    gsap.to(imageDivRef.current,{
-      scrollTrigger:{
-        trigger: imageDivRef.current,
-        markers: true
-      }
-    })
-  })
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+
+  const imageArray = [
+    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007',
+    'https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+  ]
+  useGSAP(() => {
+  gsap.to(imageDivRef.current, {
+    scrollTrigger: {
+      trigger: imageDivRef.current,
+      markers: true,
+      start: "top 24%",
+      end: "top -100%",
+      scrub:1,
+      pin: true,
+      onUpdate: (elem) => {
+        let imageIdx;
+        if(elem.progress<1){
+          imageIdx = Math.floor(elem.progress * imageArray.length);
+        }
+        else{
+          imageIdx = imageArray.length-1
+        }
+        imageRef.current.src = imageArray[imageIdx];
+      },
+    },
+  });
+}, { scope: imageDivRef });
+
 
   return (
     <div>
       <div className='section1'>
         <div ref={imageDivRef} className='absolute overflow-hidden rounded-2xl h-[20vw] w-[15vw] top-45 left-[30vw]'>
-          <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
         </div>
         <div className='relative font-[font1]'>
           <div className='mt-[58vh]'>
