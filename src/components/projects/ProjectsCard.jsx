64dc28e526129fd6b898458bce92fb82e671e2ce
Balia077 <balaramd894@gmail.com>
2025-12-25 16:39:24 +0530

    feat: add ProjectsCard component; implement project display with images and hover effects

---
+++ src/components/projects/ProjectsCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ProjectsCard = (props) => {
+    return (
+        <>
+        <div className='cursor-pointer w-1/2 h-full group relative transition-all hover:rounded-[50px] overflow-hidden'>
+                <img className='h-full w-full object-cover' src={props.image1} alt="" />
+                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10'>
+                    <h2 className='uppercase text-[4vw] px-4 pt-1.5 leading-12 text-white border-white font-[font2] border-2 rounded-full '>Vior le projet</h2>
+                </div>
+            </div>
+            <div className='cursor-pointer w-1/2 h-full group relative transition-all hover:rounded-[50px] overflow-hidden'>
+                <img className='h-full w-full object-cover' src={props.image2} alt="" />
+                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10'>
+                    <h2 className='uppercase text-[4vw] px-4 pt-1.5 leading-12 text-white border-white font-[font2] border-2 rounded-full '>Vior le projet</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectsCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,57 @@
 import React from 'react'
+import ProjectsCard from '../components/projects/ProjectsCard'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 const Projects = () => {
+
+  const projects = [{
+    image1: 'https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93',
+    image2: 'https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b'
+  },
+  {
+    image1: 'https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e',
+    image2: 'https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022'
+  },
+  {
+    image1: 'https://k72.ca/images/caseStudies/COUP_FUMANT/CF_thumbnail.jpg?w=1280&h=960&s=c119303a20520c4188aa3f592038fd4c',
+    image2: 'https://k72.ca/images/caseStudies/SHELTON/thumbnailimage_shelton.jpg?w=1280&h=960&s=63d0eaa180cbc02d3ada285ad9ef1479'
+  },
+  {
+    image1: 'https://k72.ca/images/caseStudies/Opto/thumbnailimage_opto.jpg?w=1280&h=960&s=938f0bfb3de1ff2a2846b884eec2d757',
+    image2: 'https://k72.ca/images/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail.jpg?w=1280&h=960&s=5c944bb014f8643227ad7bb117fccc14'
+  }
+  ]
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function(){
+    gsap.from('.hero',{
+      height:'150px',
+      stagger:{
+        amount:0.3
+      },
+      scrollTrigger:{
+        trigger:'.lol',
+        start:'top 100%',
+        end:'top -350%',
+        scrub: true
+      }
+    })
+  })
+
   return (
-    <div className='p-2.5'>
+    <div className='p-2.5 mb-[100vh]'>
       <div className='pt-[40.5vh]'>
         <h2 className='font-[font2] text-[13vw] uppercase'>Projets</h2>
       </div>
-      <div>
-        <div className='w-full h-[550px] mb-3 gap-3 flex -mt-20'>
-          <div className='w-1/2 h-full hover:rounded-4xl overflow-hidden'>
-            <img className='h-full w-full object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="" />
+      <div className='-mt-20 lol'>
+        {projects.map(function (elem,idx) {
+          return <div key={idx} className='hero w-full h-[550px] mb-3 gap-3 flex'>
+            <ProjectsCard image1={elem.image1} image2={elem.image2} />
           </div>
-          <div className='w-1/2 h-full hover:rounded-4xl overflow-hidden'>
-            <img className='h-full w-full object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
-          </div>
-        </div>
+        })}
       </div>
     </div>
   )
