33a485a9277814d163b6997f119dbc1ad92ed504
Balia077 <balaramd894@gmail.com>
2025-12-27 11:41:48 +0530

    feat: enhance ProjectNav component; add routing links and improve hover effects

--- src/components/projects/ProjectNav.jsx
+++ src/components/projects/ProjectNav.jsx
@@ -1,12 +1,16 @@
 import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
 
 const ProjectNav = () => {
 
     const NavGreenRef = useRef(null)
     const NavGreenRef2 = useRef(null)
+    const NavGreenRef3 = useRef(null)
+    const NavLinesRef = useRef(null)
+    const NavLinesRef2 = useRef(null)
 
   return (
-    <div className=' w-full flex font-[font2] top-0 items-start justify-end'>
+    <div className='w-full flex font-[font2] top-0 items-start justify-end'>
       <div onMouseEnter={()=>{
         NavGreenRef.current.style.height = '100%';
         NavGreenRef.current.style.color = 'black';
@@ -16,7 +20,7 @@ const ProjectNav = () => {
         NavGreenRef.current.style.color = 'white';
       }}
       className='relative lg:h-[3.5vw] h-0 lg:w-[20vw] text-white bg-black flex '>
-      <div ref={NavGreenRef} className='absolute h-0 w-full bg-[#D3FD50] text-white transition-all lg:text-[1.4vw] text-[0px]'><h2 className='absolute top-5 px-3 uppercase'>project(17)</h2></div>
+      <div ref={NavGreenRef} className='absolute h-0 w-full bg-[#D3FD50] text-white transition-all lg:text-[1.4vw] text-[0px] cursor-pointer'><Link className='h-full w-full absolute py-5 px-3 uppercase' to={'/projects'}>project(17)</Link></div>
       </div>
       <div onMouseEnter={()=>{
         NavGreenRef2.current.style.height = '100%';
@@ -25,12 +29,25 @@ const ProjectNav = () => {
       onMouseLeave={()=>{
         NavGreenRef2.current.style.height = '0%';
         NavGreenRef2.current.style.color = 'white';
-      }} className='relative text-white lg:h-[5.5vw] h-0 lg:w-[30vw] bg-black flex lg:text-[1.4vw]'>
-      <div ref={NavGreenRef2} className='absolute h-0 w-full bg-[#D3FD50] transition-all'><h2 className='absolute top-13 px-4 uppercase'>agence</h2></div>
+      }} className='relative text-white lg:h-[5.5vw] h-0 lg:w-[30vw] bg-black flex'>
+      <div ref={NavGreenRef2} className='absolute h-0 w-full bg-[#D3FD50] text-white transition-all lg:text-[1.4vw] text-[0px] cursor-pointer'><Link className='w-full h-full absolute px-3 uppercase leading-34' to={'/agence'}>agence</Link></div>
       </div>
-      <div className='text-white lg:h-[7.5vw] lg:w-[15vw] h-[12vw] w-[50vw] bg-black flex lg:text-[1.4vw]'><h2 className='py-21 p-3 uppercase'>menu</h2>
-        <div className='bg-white lg:h-[0.1vw] lg:w-[3vw] h-[0.5vw] w-[10vw] mt-5 right-8 absolute'></div>
-        <div className='bg-white lg:h-[0.1vw] lg:w-[1.5vw] h-[0.5vw] w-[6vw] mt-6 right-8 absolute'></div>
+      <div onMouseEnter={()=>{
+        NavGreenRef3.current.style.height = '100%';
+        NavGreenRef3.current.style.color = 'black';
+        NavLinesRef.current.style.backgroundColor = 'black';
+        NavLinesRef2.current.style.backgroundColor = 'black';
+      }}
+      onMouseLeave={()=>{
+        NavGreenRef3.current.style.height = '0%';
+        NavGreenRef3.current.style.color = 'white';
+        NavLinesRef.current.style.backgroundColor = 'white';
+        NavLinesRef2.current.style.backgroundColor = 'white';
+      }}
+      className='relative text-white lg:h-[7.5vw] lg:w-[15vw] h-[12vw] w-[50vw] bg-black flex lg:text-[1.4vw]'>
+        <div ref={NavGreenRef3} className='text-white h-0 w-full bg-[#D3FD50] transition-all cursor-pointer absolute'><span className='absolute px-3 uppercase w-full h-full leading-50'>menu</span></div>
+        <div ref={NavLinesRef2} className='bg-white lg:h-[0.1vw] lg:w-[1.5vw] h-[0.5vw] w-[6vw] mt-6 right-8 absolute'></div>
+        <div ref={NavLinesRef} className='bg-white lg:h-[0.1vw] lg:w-[3vw] h-[0.5vw] w-[10vw] mt-5 right-8 absolute'></div>
       </div>
     </div>
   )
